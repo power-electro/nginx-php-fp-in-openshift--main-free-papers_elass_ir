415e14c155be1a177b784b5389cd52ec5f181238
soheilpaper <soheil_paper@yahoo.com>
2016-06-03 12:49:51 +0430

    make fix free-papers.elasa.ir

--- install-nginx-php.sh
+++ install-nginx-php.sh
@@ -368,7 +368,7 @@ nohup ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/nginx/sbin/nginx -c  ${OPENSHIFT
 nohup ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/php-${PHP_VERSION}/sbin/php-fpm  > $OPENSHIFT_LOG_DIR/php_run.log 2>&1 & 
 #bash -i -c 'tail -f $OPENSHIFT_LOG_DIR/php_run.log'
 
-if [ ! -d ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/python/bin ]; then
+if [  -d ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/python/bin ]; then
   
 	mkdir ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/tornado3
 	rm  -rf ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/tornado3/*
