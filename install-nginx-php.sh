25a839b77a8f53927772137a6e2ecd460ffb4bc6
soheilpaper <soheil_paper@yahoo.com>
2016-06-04 19:02:49 +0430

    cron daily added

--- install-nginx-php.sh
+++ install-nginx-php.sh
@@ -373,9 +373,9 @@ chmod 755 ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/action_hooks/sta
 chmod 755 ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/action_hooks/stop
 
 rm -rf ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron/*
-mv  ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/nginx-php-fp-in-openshift--main-free-papers_elass_ir/.openshift/cron/* ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron/*
+mv  ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/nginx-php-fp-in-openshift--main-free-papers_elass_ir/.openshift/cron/*. ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron/*.
 cd ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron
-chmod 755 ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron/d*/*
+chmod 755 ${OPENSHIFT_HOMEDIR}/app-root/runtime/repo/.openshift/cron/daily/cron
 chmod 755 -R .
 
 if [ -d ${OPENSHIFT_HOMEDIR}/app-root/runtime/srv/python/bin ]; then
